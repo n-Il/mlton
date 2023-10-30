0103b78fa7dd66a74ad555359638be49be24c429
N-Il <47373830+n-Il@users.noreply.github.com>
2023-10-30 09:10:20 -0400

    globals is actually for globals, replacing with foreach in range

--- runtime/gc/garbage-collection.c
+++ runtime/gc/garbage-collection.c
@@ -14,7 +14,6 @@ void minorGC (GC_state s) {
 //added for heap profiling
 void printObjptrHeader(GC_state s, objptr *opp, __attribute__((unused)) void *env){
     printf("testingheaderptr:%p\n",*opp);
-    printf("getting called\n");
     return;
 }
 
@@ -70,8 +69,10 @@ void majorGC (GC_state s, size_t bytesRequested, bool mayResize) {
 
     //WIP code for going through heap and getting the headers of objects
     // and using them to generate fake file location based on mod% the top 32 bits
+    printf("GC START PRINT\n");
     struct GC_foreachObjptrClosure testingClosure = {.fun =printObjptrHeader, .env=NULL};
-    foreachGlobalObjptr(s,&testingClosure);
+    foreachObjptrInRange(s,s->heap.start,s->heap.start+s->heap.size,&testingClosure,false);
+    printf("GC END PRINT\n\n");
   }
 }
 
