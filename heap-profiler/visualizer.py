5effcaf42c5ef4d70ad75ab3ca24b5afd0aa09ff
N-Il <47373830+n-Il@users.noreply.github.com>
2023-12-01 11:42:48 -0500

    dict form added

--- heap-profiler/visualizer.py
+++ heap-profiler/visualizer.py
@@ -1,5 +1,6 @@
-#Matt Harris
-#This code reads in the binary data and graphs it using matplotlib
+### Matt Harris
+### This code reads in the binary data and graphs it using matplotlib.
+###
 import sys
 import os
 import matplotlib.pyplot as plt
@@ -10,46 +11,72 @@ import matplotlib.dates
 from datetime import datetime,timezone,timedelta
 
 
+### The binary data reading code is not perfectly portable, it is dependant on the system's C datatype sizes
+### It is not impossible to solve this issue in the future.
+BOOL_BYTE_SIZE = 1
+INT_BYTE_SIZE = 4
+UINT32_T_BYTE_SIZE = 4
+SIZE_T_BYTE_SIZE = 8
+UINTMAX_T_BYTE_SIZE = 8
 
-
-def parse_data(location):
+#reads the profiling data from file to dictionary
+def read_data(location, debug = False):
+    data = dict()
     with open(location,"rb") as f:
         #bool location profiling
-        location_profiling_bool = int.from_bytes(f.read(1),"little") 
-        print("location_profiling_bool:"+str(location_profiling_bool))
+        location_profiling_bool = int.from_bytes(f.read(BOOL_BYTE_SIZE),"little") 
         #bool lifetime profiling
-        lifetime_profiling_bool = int.from_bytes(f.read(1),"little") 
-        print("lifetime_profiling_bool:"+str(lifetime_profiling_bool))
+        lifetime_profiling_bool = int.from_bytes(f.read(BOOL_BYTE_SIZE),"little") 
         #int accuracy
-        lifetime_accuracy_int = int.from_bytes(f.read(4),"little") 
-        print("lifetime_accuracy_int:"+str(lifetime_accuracy_int))
+        lifetime_accuracy_int = int.from_bytes(f.read(INT_BYTE_SIZE),"little") 
         #uint32_t sourcenameslength
-        location_source_names_length_uint32_t = int.from_bytes(f.read(4),"little") 
-        print("location_source_names_length_uint32_t:"+str(location_source_names_length_uint32_t))
+        location_source_names_length_uint32_t = int.from_bytes(f.read(UINT32_T_BYTE_SIZE),"little") 
+        data["location_profiling"] = True if location_profiling_bool == 1 else False
+        data["lifetime_profiling"] = True if lifetime_profiling_bool == 1 else False
+        data["lifetime_accuracy"] = lifetime_accuracy_int
+        data["source_names_length"] = location_source_names_length_uint32_t
+        if debug:
+            print("location_profiling_bool:"+str(location_profiling_bool))
+            print("lifetime_profiling_bool:"+str(lifetime_profiling_bool))
+            print("lifetime_accuracy_int:"+str(lifetime_accuracy_int))
+            print("location_source_names_length_uint32_t:"+str(location_source_names_length_uint32_t))
         #if location profiling is true then 
         if location_profiling_bool:
+            source_names = []
             #for x from 0 to sourceNamesLength
             for x in range(location_source_names_length_uint32_t):
                 #size_t len
-                source_string_len_size_t = int.from_bytes(f.read(8),"little")
-                print("source_string_len_size_t:"+str(source_string_len_size_t))
+                source_string_len_size_t = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little")
                 #bytes of string
                 source_name_string = f.read(source_string_len_size_t).decode()
-                print("source_name_string:"+source_name_string)
-        counter = 1
-        while (b := f.read(8)):
+                source_names.append(source_name_string)
+                if debug:
+                    print("source_string_len_size_t:"+str(source_string_len_size_t))
+                    print("source_name_string:"+source_name_string)
+            data["source_names"] = source_names
+        counter = 0
+        garbage_collections = []
+        while (b := f.read(UINTMAX_T_BYTE_SIZE)): 
+            counter += 1
+            gc = dict()
+            gc["#"] = counter
             #uintmax_t time in ms
             time_in_ms_uintmax_t = int.from_bytes(b,"little")
-            print("time_in_ms_uintmax_t:"+str(time_in_ms_uintmax_t))
             #size_t utilization
-            heap_live_bytes_size_t = int.from_bytes(f.read(8),"little")
-            print("heap_live_bytes_size_t:"+str(heap_live_bytes_size_t))
+            heap_live_bytes_size_t = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little")
             #size_t heap total size
-            heap_total_size_size_t = int.from_bytes(f.read(8),"little")
-            print("heap_total_size_size_t:"+str(heap_total_size_size_t))
+            heap_total_size_size_t = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little")
             #size_t object count
-            heap_num_objects_size_t = int.from_bytes(f.read(8),"little")
-            print("heap_num_objects_size_t:"+str(heap_num_objects_size_t))
+            heap_num_objects_size_t = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little")
+            gc["time_ms"] = time_in_ms_uintmax_t
+            gc["live_data"] = heap_live_bytes_size_t
+            gc["total_size"] = heap_total_size_size_t
+            gc["num_objects"] = heap_num_objects_size_t
+            if debug:
+                print("time_in_ms_uintmax_t:"+str(time_in_ms_uintmax_t))
+                print("heap_live_bytes_size_t:"+str(heap_live_bytes_size_t))
+                print("heap_total_size_size_t:"+str(heap_total_size_size_t))
+                print("heap_num_objects_size_t:"+str(heap_num_objects_size_t))
             #if gc survived
             if (lifetime_profiling_bool == 1):
                 #lifetimes 1 2 3 4 5 <10 <100 <1000 <10000 <100000 <1000000 <10000000 and longer
@@ -57,29 +84,37 @@ def parse_data(location):
                 sum_size_objects_per_lifetime = [0] * 13
                 #read 13 size_t #obj array
                 for i in range(13):
-                    num_objects_per_lifetime[i] = int.from_bytes(f.read(8),"little") 
-                print("num_objects_per_lifetime:"+str(num_objects_per_lifetime))
+                    num_objects_per_lifetime[i] = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little") 
                 #read 13 size_t sumsizeobj array
                 for i in range(13):
-                    sum_size_objects_per_lifetime[i] = int.from_bytes(f.read(8),"little") 
-                print("sum_size_objects_per_lifetime:"+str(sum_size_objects_per_lifetime))
+                    sum_size_objects_per_lifetime[i] = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little") 
+                gc["objects_per_lifetime"] = num_objects_per_lifetime
+                gc["bytes_per_lifetime"] = sum_size_objects_per_lifetime
+                if debug:
+                    print("num_objects_per_lifetime:"+str(num_objects_per_lifetime))
+                    print("sum_size_objects_per_lifetime:"+str(sum_size_objects_per_lifetime))
             #if location
             if (location_profiling_bool == 1):
                 #for x from 0 to sourceNamesLength
                 num_objects_per_location = [0] * location_source_names_length_uint32_t
                 sum_size_objects_per_location = [0] * location_source_names_length_uint32_t
-
                 for i in range(location_source_names_length_uint32_t):    
                     # number objects with this source size_t
-                    num_objects_per_location[i] = int.from_bytes(f.read(8),"little") 
+                    num_objects_per_location[i] = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little") 
                     # sum size of objects with source  size_t
-                    sum_size_objects_per_location[i] = int.from_bytes(f.read(8),"little") 
-                print("num_objects_per_location:"+str(num_objects_per_location))
-                print("sum_size_objects_per_location:"+str(sum_size_objects_per_location))
-        ba = bytearray(f.read())
-        for b in ba:
-            print("leftover bytes")
-    return
+                    sum_size_objects_per_location[i] = int.from_bytes(f.read(SIZE_T_BYTE_SIZE),"little") 
+                gc["objects_per_location"] = num_objects_per_location
+                gc["bytes_per_location"] = sum_size_objects_per_location
+                if debug:
+                    print("num_objects_per_location:"+str(num_objects_per_location))
+                    print("sum_size_objects_per_location:"+str(sum_size_objects_per_location))
+            garbage_collections.append(gc) 
+        data["garbage_collections"] = garbage_collections
+        if debug:
+            ba = bytearray(f.read())
+            for b in ba:
+                print("leftover byte")
+    return data
 
 #TODO: cut this out after working
 #def read_in_data(location):
@@ -107,10 +142,10 @@ def main():
     if len(sys.argv) < 2:
         print("give the file location that you output using @MLton heap-profiling <filename> --")
     else:
-        parse_data(sys.argv[1])
-        #datapoints = read_in_data(sys.argv[1])
-        #graph_one(datapoints)
-        #graph_five(datapoints)
+        data = read_data(sys.argv[1])
+        #normal graphs 
+        #if loc then those graphs
+        #if life then those graphs
     return
 
 
